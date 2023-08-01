83b6378be932a3ecbb39bfb2257280423318f50c
Anthony Cueva <hi.cuevantn@gmail.com>
2023-08-01 01:27:02 -0500

    feat: not just a component, it's a page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,70 +6,92 @@
 	import { draggable, dropzone } from '$lib/dnd';
 </script>
 
-<ul>
-	{#each data.columns as column}
-		{@const cards = data.cards.filter((c) => c.column === column.id)}
-		<li class="column">
-			<h2>{column.label}</h2>
-			{#if cards.length > 0}
-				<ul class="cards">
-					{#each cards as card}
-						<li
-							class="dropzone"
-							use:dropzone={{
-								dropzoneClass: 'dropzone',
-								dragoverClass: 'dragover',
-								on_drop(draggable_id) {
-									const dropzone_index_original = data.cards.findIndex((c) => c.id === card.id);
-									const draggable_index_original = data.cards.findIndex(
-										(c) => c.id === draggable_id
-									);
+<main>
+	<h1 class="principal">A Kanban board</h1>
+	<ul class="principal">
+		{#each data.columns as column}
+			{@const cards = data.cards.filter((c) => c.column === column.id)}
+			<li class="column">
+				<h2>{column.label}</h2>
+				{#if cards.length > 0}
+					<ul class="cards">
+						{#each cards as card}
+							<li
+								class="dropzone"
+								use:dropzone={{
+									dropzoneClass: 'dropzone',
+									dragoverClass: 'dragover',
+									on_drop(draggable_id) {
+										const dropzone_index_original = data.cards.findIndex((c) => c.id === card.id);
+										const draggable_index_original = data.cards.findIndex(
+											(c) => c.id === draggable_id
+										);
 
-									let arr = [...data.cards];
-									arr.splice(draggable_index_original, 1);
+										let arr = [...data.cards];
+										arr.splice(draggable_index_original, 1);
 
-									const dropzone_index_new = arr.findIndex((c) => c.id === card.id);
+										const dropzone_index_new = arr.findIndex((c) => c.id === card.id);
 
-									for (let i = arr.length; i > dropzone_index_new; i--) {
-										arr[i] = arr[i - 1];
+										for (let i = arr.length; i > dropzone_index_new; i--) {
+											arr[i] = arr[i - 1];
+										}
+
+										arr[dropzone_index_new] = {
+											...data.cards[draggable_index_original],
+											column: data.cards[dropzone_index_original].column
+										};
+
+										console.log({
+											original: data.cards,
+											new: arr
+										});
+
+										data = {
+											columns: data.columns,
+											cards: arr
+										};
 									}
-
-									arr[dropzone_index_new] = {
-										...data.cards[draggable_index_original],
-										column: data.cards[dropzone_index_original].column
-									};
-
-									console.log({
-										original: data.cards,
-										new: arr
-									});
-
-									data = {
-										columns: data.columns,
-										cards: arr
-									};
-								}
-							}}
-						>
-							<div class="drop-signal" />
-							<div class="card" use:draggable={card.id}>{card.title}</div>
-						</li>
-					{/each}
-				</ul>
-			{:else}
-				<p>No Cards...</p>
-			{/if}
-		</li>
-	{/each}
-</ul>
+								}}
+							>
+								<div class="drop-signal" />
+								<div class="card" use:draggable={card.id}>{card.title}</div>
+							</li>
+						{/each}
+					</ul>
+				{:else}
+					<p>No Cards...</p>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+	<p class="principal">
+		It's an Svelte Kanban board app with native drag and drop support. Work in progress. Check the
+		code on <a href="https://github.com/cuevantn/kanban">github</a>.
+	</p>
+</main>
 
 <style>
+	main {
+		margin: 0 auto;
+		width: min-content;
+	}
+
 	ul {
 		list-style: none;
-		margin: 0;
 		padding: 0;
 		display: flex;
-		gap: 0 1rem;
+		gap: 0 4rem;
+	}
+
+	.principal {
+		border: 0.125rem solid hsl(var(--sk-back-4));
+		padding: 4rem;
+		border-radius: 0.5rem;
+	}
+
+	h1 {
+		text-align: center;
+		margin-bottom: 2rem;
 	}
 
 	.column {
@@ -90,23 +112,27 @@
 	}
 
 	.card {
-		background-color: var(--sk-back-1);
+		background-color: hsl(var(--sk-back-1));
 		padding: 1rem;
 		border: 1px solid black;
 		border-radius: 0.5rem;
-		border-color: var(--sk-back-5);
+		border-color: hsl(var(--sk-back-5));
 	}
 
 	.drop-signal {
 		width: 100%;
 		height: 0.25rem;
 		border-radius: 9999px;
-		background-color: var(--sk-theme-1);
+		background-color: hsl(var(--sk-theme-1));
 		opacity: 0;
 		margin: 0.25rem 0;
 	}
 
 	.dropzone:global(.dragover) .drop-signal {
 		opacity: 1;
+	}
+
+	.card:active {
+		background-color: hsla(var(--sk-back-1), 0.3);
 	}
 </style>
